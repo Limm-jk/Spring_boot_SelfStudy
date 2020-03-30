795dabd9bbd2743d0166872bc8d71811672f2874
Limm-jk <201602057@cs-cnu.org>
2020-03-30 22:44:31 +0900

    first window

--- src/main/java/com/example/test/DemoApplication.java
+++ src/main/java/com/example/test/DemoApplication.java
@@ -1,10 +1,25 @@
 package com.example.test;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+	@RequestMapping("/")
+	public String home() {
+	 return "Hello Spring Boot";
+	}
+	// 추가로 신규 url 추가
+	@RequestMapping("/new")
+	public String newMethod() {
+	 return "New Url";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
